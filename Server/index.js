c5539270a54823d66c2ff516fd878573ada2f00c
devcowsar <kawsarcse836@gmail.com>
2024-04-29 10:33:50 +0600

    fix: server

--- Server/index.js
+++ Server/index.js
@@ -9,7 +9,8 @@ const app = express()
 app.use(cors({
     origin: ["http://localhost:5173"],
     methods: ['GET', 'POST', 'PUT', "DELETE"],
-    credentials: true
+    credentials: true,
+    origin: 'https://yourdeployedsite.com'
 }))
 app.use(express.json())
 app.use(cookieParser())
